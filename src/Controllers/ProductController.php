4c8d6bcc2b5717451f2b106f2f19f309b7412b12
kareem shousha <kemoshousha309@gmail.com>
2024-09-13 18:22:19 +0300

    Add router and product controller

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -1,36 +1,73 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controllers;
 
-class ProductController
+use App\Exceptions\ValidationException;
+use App\Mappers\ProductMapper;
+use App\Services\ProductServices\Interfaces\CreateProductServiceInterface;
+use App\Services\ProductServices\Interfaces\DeleteProductServiceInterface;
+use App\Services\ProductServices\Interfaces\ListProductServiceInterface;
+
+// handle protocol logic and call the right service 
+class ProductController implements ControllerInterface
 {
-    public function index()
+    private CreateProductServiceInterface $createService;
+    private ListProductServiceInterface $listProductService;
+    private DeleteProductServiceInterface $deleteProductService;
+    private ProductMapper $mapper;
+
+
+    public function __construct(
+        CreateProductServiceInterface $createService, 
+        ListProductServiceInterface $listProductService,
+        DeleteProductServiceInterface $deleteProductService, 
+        ProductMapper $mapper)
     {
-        // Fetch all products
-        echo json_encode(['message' => 'List all products']);
+        $this->createService = $createService;
+        $this->listProductService = $listProductService;
+        $this->deleteProductService = $deleteProductService;
+        $this->mapper = $mapper;
     }
 
-    public function create()
+    public function list(): void
     {
-        // Create a new product
-        echo json_encode(['message' => 'Product created']);
+        $this->listProductService->list();
+    } 
+
+    public function create(): void 
+    {
+        $jsonData = file_get_contents("php://input");
+        $postData = json_decode($jsonData, true);
+        if(!$postData) {
+            throw new ValidationException(["Type Error"=>"you should provide product data"]);
+        }
+        $productDto = $this->mapper->toCreateProductDto($postData);
+        $this->createService->create($productDto);
     }
 
-    public function show($id)
+    public function delete(string $id): void
+    {
+        $this->deleteProductService->delete($id);
+    }
+
+    public function deleteAll(): void
+    {
+        $this->deleteProductService->deleteAll();
+    }
+
+    public function show($id): void
     {
         // Show a specific product
         echo json_encode(['message' => "Show product with ID: $id"]);
     }
 
-    public function update($id)
+    public function update($id): void
     {
         // Update an existing product
         echo json_encode(['message' => "Update product with ID: $id"]);
     }
 
-    public function delete($id)
-    {
-        // Delete a product
-        echo json_encode(['message' => "Delete product with ID: $id"]);
-    }
+
 }
--- src/Router.php
+++ src/Router.php
@@ -1,12 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
+use App\Controllers\ControllerInterface;
+use App\DTOs\Response;
+use App\Exceptions\ValidationException;
+use Throwable;
+
+// direct the endpoint call to the right controller and method 
 class Router
 {
     private array $routes = [];
 
-    public function add(string $endpoint, string $controller, string $method, string $httpMethod = 'GET')
+    public function add(string $endpoint, ControllerInterface $controller, string $method, string $httpMethod = 'GET')
     {
         $this->routes[] = [
             'endpoint' => $endpoint,
@@ -24,23 +32,36 @@ class Router
         if (strpos($path, $basePath) === 0) {
             $path = substr($path, strlen($basePath));
         }
-        
+
         $path = rtrim($path, '/');
         $requestMethod = $_SERVER['REQUEST_METHOD'];
-        
+
         foreach ($this->routes as $route) {
             // Simple pattern matching for dynamic routes (e.g., /products/{id})
-            $pattern = "@^" . preg_replace('/\{[^\}]+\}/', '([A-Za-z0-9]+)', $route['endpoint']) . "$@";
+            $pattern = "@^" . preg_replace('/\{[^\}]+\}/', '([A-Za-z0-9\-]+)', $route['endpoint']) . "$@";
 
-            if (preg_match($pattern, $path, $matches) && $route['httpMethod'] === $requestMethod) {
-                $controller = new $route['controller']();
-                array_shift($matches); // Remove the full match from the matches array
-                return call_user_func_array([$controller, $route['method']], $matches);
-            }
+            try {
+                if (preg_match($pattern, $path, $matches) && $route['httpMethod'] === $requestMethod) {
+                    $controller = $route['controller'];
+                    array_shift($matches); // Remove the full match from the matches array
+                    return call_user_func_array([$controller, $route['method']], $matches);
+                }
+            }catch (ValidationException $err) {
+                http_response_code(400);
+                $res = new Response(["Validation Error" => $err->getValidation()]);
+                echo $res->jsonResponse();
+                return;
+            } catch (Throwable $error) {
+                http_response_code(500);
+                $res = new Response(["Error" => "Internal Server Error", "details" => $error->getMessage()]); 
+                echo $res->jsonResponse();
+                return;
+            } 
         }
 
         // Fallback to a 404 page if no route is found
         http_response_code(404);
-        echo json_encode(['error' => 'Not Found']);
+        $res = new Response(['Error' => 'Not Found']);
+        echo $res->jsonResponse();
     }
 }
