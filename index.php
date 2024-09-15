464994c36e96294f8cf5a13f9965b3e3b6d0dc92
kareem shousha <kemoshousha309@gmail.com>
2024-09-15 10:16:14 +0300

    update the headers

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Specify allowed HTTP methods
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Specify allowed headers
+// Handle preflight requests (CORS OPTIONS request)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 
 use App\Router;
 use App\Controllers\ProductController;
