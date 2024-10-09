27741534e59d96461131b5d1cd1a290c02a19b95
kareem shousha <kemoshousha309@gmail.com>
2024-10-09 20:25:02 +0300

    replace switch conditions with a map

--- src/Factories/ProductCreatorSelector.php
+++ src/Factories/ProductCreatorSelector.php
@@ -5,22 +5,26 @@ declare(strict_types=1);
 namespace App\Factories;
 
 use App\Exceptions\ValidationException;
+use ErrorException;
 
 class ProductCreatorSelector
 {
+    // on having more types just add a new record to the registry
+    private array $productCreatorRegistry = [
+        "book" => "App\Factories\BookProductCreator",
+        "furniture" => "App\Factories\FurnitureProductCreator",
+        "dvd" => "App\Factories\DvdProductCreator",
+    ];
     public function selectCreator(string $type): IProductCreator
     {
-        // this condition logic could be done by switch, if, hashTable(map), ...
-        switch ($type) {
-            case 'book':
-                return new BookProductCreator();
-            case 'dvd':
-                return new DvdProductCreator();
-            case 'furniture':
-                return new FurnitureProductCreator();
-            default:
-                throw new ValidationException(["Type Error" => 'please provide a correct type']);
-                break;
+        try {
+            if (!isset($this->productCreatorRegistry[$type])) {
+                throw new ErrorException();
+            }
+            $creator = new $this->productCreatorRegistry[$type]();
+            return $creator;
+        } catch (ErrorException $err) {
+            throw new ValidationException(["Type Error" => 'please provide a correct type']);
         }
     }
 }
